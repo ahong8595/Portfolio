eb4d7e546b904cd6d99ba12067383a8650800251
Andrew Hong <ahong8595@gmail.com>
2022-08-20 16:59:37 -0400

    fix navbar

--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -1,8 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image'
 import React from 'react';
 
+import github from "../data/images/gitHub.png";
+import linkedin from "../data/images/linkedin.png";
 import "./style/MainNavBar.css";
 
 class MainNavBar extends React.Component {
@@ -15,6 +18,12 @@ class MainNavBar extends React.Component {
                     <Nav className="ms-auto">
                         <Nav.Link className="link" href="/projects">Projects</Nav.Link>
                         <Nav.Link className="link" href="/resume">Résume</Nav.Link>
+                        <Nav.Link className="link" href="https://github.com/ahong8595">
+                            <Image className="image" src={github}></Image>
+                        </Nav.Link>
+                        <Nav.Link className="link" href="https://www.linkedin.com/in/andrew-hong-a99330149/">
+                            <Image className="image" src={linkedin}></Image>
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
